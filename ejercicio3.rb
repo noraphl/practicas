d164197cf753bbbef0bd5c0a1bc94ae6c6b6f47a
Nora Hernandez <norap_hdezl@hotmail.com>
2017-05-24 13:30:27 -0500

    Commit

---
+++ ejercicio3.rb
@@ -0,0 +1,15 @@
+title = "Título del post"
+puts title
+description = "Descrípción del post"
+puts description
+number_9f_likes = 10
+puts number_9f_likes
+NUM_OF_POST = 1
+puts NUM_OF_POST
+
+street = "Calle principal"
+number = "207"
+state = "Puebla"
+city = "Cholula"
+zip = "75841"
+puts "Dirección: #{street} #{number} #{city}, #{state} C.P. #{zip}"
---
+++ ejercicio6.rb
@@ -0,0 +1,5 @@
+a = 'hola mundo'
+puts a
+puts a[2,4]
+puts a[/la/]
+puts a[/sol/, 0]
